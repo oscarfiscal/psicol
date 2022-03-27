413dd6aa1d251410146059fcdffeeeaf4e91d3be
oscarfiscal <oscarfiscalcp@gmail.com>
2022-03-26 22:27:16 -0500

    listar compradores

--- app/Http/Resources/BuyerCollection.php
+++ app/Http/Resources/BuyerCollection.php
@@ -14,6 +14,7 @@ class BuyerCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
-    }
+        return[
+            'data'=>$this->collection,
+            ];    }
 }
